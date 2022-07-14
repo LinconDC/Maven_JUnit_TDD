c90834d53033f6adf2d7cd6525ac733b9cc98e6b
Lincon Martins <linconmartins18@gmail.com>
2022-07-14 13:45:41 -0300

    Create automated test

--- tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -14,23 +14,25 @@ class BonusServiceTest {
 	@Test
 	void acimaDeMilBonusZero() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(
-				new Funcionario("Rodrigo", LocalDate.now(), 
-				new BigDecimal("25000")));
-		
-		
-		assertEquals(BigDecimal.ZERO, bonus);
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
+		assertEquals(new BigDecimal("0.00"), bonus);
 	}
-	
+
 	@Test
 	void bonusDezPorCentoDoSalario() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(
-				new Funcionario("Rodrigo", LocalDate.now(), 
-				new BigDecimal("2500")));
-		
-		
-		assertEquals(new BigDecimal("250"), bonus);
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
+		assertEquals(new BigDecimal("250.00"), bonus);
+	}
+
+	@Test
+	void bonusDeveriaSerDezPorCentoParaSalarioDeDezMil() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
 
 }
